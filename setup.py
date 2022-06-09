6fc4eaebc037113903d9febb70665214e93740ae
gazwald <gary.brandon@gmail.com>
2022-06-09 18:54:46 +1000

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="finder",
+    version="1.0",
+    description="Utility for finding/replacing items in a dict",
+    author="Gary Brandon",
+    author_email="gary.brandon@gmail.com",
+    license="BSD",
+    url="https://github.com/gazwald/finder",
+)
